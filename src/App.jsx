758cc46bffab3950837932547b097ddff92c417d
Damien FAY <Damien.fay@etudiant.univ-reims.fr>
2024-03-04 09:46:59 +0100

    FIN TP1

--- src/App.jsx
+++ src/App.jsx
@@ -4,92 +4,108 @@ import { faShieldCat } from "@fortawesome/free-solid-svg-icons";
 import Card from "./components/Card";
 import FoldableCard from "./components/FoldableCard";
 import Counter from "./components/Counter";
-import data from "./constants/cardsData.js";
+import Data from "./constants/cardsData";
+import Cards from "./components/Cards";
+
+const test = 2;
 
 function App() {
-  const [gates, setGates] = useState({
-    one: false,
-    two: false,
-    three: false,
-  });
+  if (test === 1) {
+    const [gates, setGates] = useState({
+      one: false,
+      two: false,
+      three: false,
+    });
 
-  const [counts, setCounts] = useState({
-    simple: 0,
-    chats: 0,
-  });
+    const [counts, setCounts] = useState({
+      simple: 0,
+      chats: 0,
+    });
 
-  const { simple, chats } = counts;
-  const { one, two, three } = gates;
+    const { simple, chats } = counts;
+    const { one, two, three } = gates;
 
-  const simpleHandler = (cpt) => {
-    setCounts({ simple: cpt, chats });
-  };
-  const chatsHandler = (cpt) => {
-    setCounts({ simple, chats: cpt });
-  };
+    const simpleHandler = (cpt) => {
+      setCounts({ simple: cpt, chats });
+    };
+    const chatsHandler = (cpt) => {
+      setCounts({ simple, chats: cpt });
+    };
 
-  const gatesHandler1 = () => {
-    if (gates.one) {
-      setGates({ one: false, two, three });
-    } else {
-      setGates({ one: true, two, three });
-    }
-  };
-  const gatesHandler2 = () => {
-    if (gates.two) {
-      setGates({ one, two: false, three });
-    } else {
-      setGates({ one, two: true, three });
-    }
-  };
-  const gatesHandler3 = () => {
-    if (gates.three) {
-      setGates({ one, two, three: false });
-    } else {
-      setGates({ one, two, three: true });
-    }
-  };
+    const gatesHandler1 = () => {
+      if (gates.one) {
+        setGates({ one: false, two, three });
+      } else {
+        setGates({ one: true, two, three });
+      }
+    };
+    const gatesHandler2 = () => {
+      if (gates.two) {
+        setGates({ one, two: false, three });
+      } else {
+        setGates({ one, two: true, three });
+      }
+    };
+    const gatesHandler3 = () => {
+      if (gates.three) {
+        setGates({ one, two, three: false });
+      } else {
+        setGates({ one, two, three: true });
+      }
+    };
 
-  // console.log(typeof chatsHandler);
+    // console.log(typeof chatsHandler);
+    return (
+      <div className="app">
+        <header className="app__header header">
+          <h1 className="header__title">Introduction to React</h1>
+        </header>
+        <main className="app__main">
+          <div className="cards">
+            <Card
+              titre="Titre 1"
+              content={
+                <Counter
+                  className="btn"
+                  onChange={simpleHandler}
+                  initial={simple}
+                />
+              }
+            />
+            <FoldableCard
+              titre="Titre 2"
+              content={
+                <Counter
+                  before={<FontAwesomeIcon icon={faShieldCat} />}
+                  after={<FontAwesomeIcon icon={faShieldCat} />}
+                  className="btn"
+                  onChange={chatsHandler}
+                  initial={chats}
+                />
+              }
+              opened={one}
+              onClick={gatesHandler1}
+            />
+            <FoldableCard
+              titre="Titre 3"
+              content={chats + simple}
+              opened={two}
+              onClick={gatesHandler2}
+            />
+            <FoldableCard titre="Rien" opened={three} onClick={gatesHandler3} />
+          </div>
+        </main>
+        <footer className="app__footer footer">footer</footer>
+      </div>
+    );
+  }
   return (
     <div className="app">
       <header className="app__header header">
         <h1 className="header__title">Introduction to React</h1>
       </header>
       <main className="app__main">
-        <div className="cards">
-          <Card
-            titre="Compteur simple"
-            content={
-              <Counter
-                className="btn"
-                onChange={simpleHandler}
-                initial={simple}
-              />
-            }
-          />
-          <FoldableCard
-            titre="Compteur avec 2 chats"
-            content={
-              <Counter
-                before={<FontAwesomeIcon icon={faShieldCat} />}
-                after={<FontAwesomeIcon icon={faShieldCat} />}
-                className="btn"
-                onChange={chatsHandler}
-                initial={chats}
-              />
-            }
-            opened={one}
-            onClick={gatesHandler1}
-          />
-          <FoldableCard
-            titre="Total compteurs"
-            content={chats + simple}
-            opened={two}
-            onClick={gatesHandler2}
-          />
-          <FoldableCard titre="Rien" opened={three} onClick={gatesHandler3} />
-        </div>
+        <Cards cardsData={Data} />
       </main>
       <footer className="app__footer footer">footer</footer>
     </div>
